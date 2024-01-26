c5c0a2d5761cc3bde6d08be8e8c55624a24a6c56
notify <notify-ctrl@qq.com>
2024-01-26 15:45:05 +0800

    大包翻译

--- init.lua
+++ init.lua
@@ -1,6 +1,8 @@
 local qyitian = require "packages/qsgs/qyitian"
 local qwisdom = require "packages/qsgs/qwisdom"
 
+Fk:loadTranslationTable{ ["qsgs"] = "太阳神三国杀" }
+
 return {
   qyitian,
   qwisdom,
